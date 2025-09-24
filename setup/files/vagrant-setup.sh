40422ae006b30e2bdf8cb40ba360e51e9d37f5b7
ruchiasagar21 <sagar.ru@northeastern.edu>
2025-09-24 12:17:16 -0400

    test

--- setup/files/vagrant-setup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-dnf install -y nano git
-dnf install python3-pip
-python3 -m pip install --user pipx
-python3 -m pipx ensurepath
-pipx install --include-deps ansible
-
-chmod ugo+x /home/vagrant/.bash*
